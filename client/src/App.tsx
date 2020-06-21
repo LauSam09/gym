4fee0927cbcded61eaaf5dba1d5d3c955ac6ffc8
Laurence Ede <laurence.ede@gmail.com>
2020-06-21 20:30:57 +0100

    Added react router.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import NavBar from './components/navbar/NavBar'
 import Workouts from './pages/workouts/Workouts'
 import styles from './App.module.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App () {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
       <section className={styles.content}>
-        <Workouts />
+        <Switch>
+          <Route path="/new">
+            <div>placeholder</div>
+          </Route>
+          <Route path="/">
+            <Workouts />
+          </Route>
+        </Switch>
       </section>
-    </div>
+    </BrowserRouter>
   )
 }
 
