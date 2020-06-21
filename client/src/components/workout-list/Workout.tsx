66abd8f81fca881b9335dc942b6f433bd0f36d55
Laurence Ede <laurence.ede@gmail.com>
2020-06-21 20:27:05 +0100

    Adjusted layout and added best set.

--- client/src/components/workout-list/Workout.tsx
+++ client/src/components/workout-list/Workout.tsx
@@ -10,7 +10,8 @@ export default function Workout (props: WorkoutProps) {
   return <div className={styles.container}>
     {props.workout.date.toDateString()}
     {props.workout.exercises?.map((exercise, index) => <div key={index}>
-      {exercise.name}
+      <span className={styles['exercise-name']}>{exercise.name}</span>
+      <span className={styles['exercise-set']}>{exercise.bestSet?.weight}kg * {exercise.bestSet?.repetitions}</span>
     </div>)}
   </div>
 }
--- client/src/pages/workouts/Workouts.tsx
+++ client/src/pages/workouts/Workouts.tsx
@@ -12,7 +12,7 @@ const workouts = [
     ]
   },
   {
-    date: new Date(2019, 6, 15, 17, 30),
+    date: new Date(2019, 6, 8, 17, 30),
     exercises: [
       { name: 'Squat', bestSet: { weight: 75, repetitions: 8 } },
       { name: 'A really really really long exercise', bestSet: { weight: 35, repetitions: 7 } }
